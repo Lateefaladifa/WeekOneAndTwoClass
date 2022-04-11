118d0db2530f9002a52908244655caee5056a400
Abdullateef Adebayo <lateefaladifa1@gmail.com>
2022-04-11 10:42:18 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WeekOneAndTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Abdullateef Adebayo";
+            int MyAge = 31;
+            double DollarToEuro = 56.98;
+            string MyWork = "Submitting my First Assignment";
+            int Amount = 123;
+            int DollarToEuroint = (int) DollarToEuro;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(MyName);
+            Console.WriteLine(MyAge);
+            Console.WriteLine(DollarToEuro);
+            Console.WriteLine(MyWork);
+            Console.WriteLine(Amount);
+            Console.WriteLine(DollarToEuro);
+        }
+    }
+}
